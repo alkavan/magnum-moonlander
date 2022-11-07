95be436dc89b53ab0628f8b40bc556248716d848
alkavan <igal@alkontek.com>
2022-11-07 11:35:36 +0200

    project init

---
+++ src/MoonLander/DrawableMesh.h
@@ -0,0 +1,40 @@
+#ifndef MAGNUM_MOONLANDER_DRAWABLEMESH_H
+#define MAGNUM_MOONLANDER_DRAWABLEMESH_H
+
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/Shaders/Flat.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/SceneGraph/Camera.h>
+#include <Magnum/SceneGraph/Drawable.h>
+#include <Magnum/SceneGraph/Scene.h>
+
+#include "Game.h"
+
+namespace Magnum::Game {
+    class DrawableMesh : SceneGraph::Drawable2D {
+    public:
+        DrawableMesh(
+                Object2D &object,
+                GL::Mesh& mesh,
+                Shaders::FlatGL2D& shader,
+                const Color4 &color,
+                SceneGraph::DrawableGroup2D &drawables
+                ) : SceneGraph::Drawable2D{object, &drawables},
+                _mesh(mesh), _shader(shader), _color(color) {}
+
+        void draw(const Matrix3 &transformationMatrix, SceneGraph::Camera2D &camera) override {
+            _shader
+                    .setTransformationProjectionMatrix(camera.projectionMatrix() * transformationMatrix)
+                    .setColor(_color);
+
+            _shader.draw(_mesh);
+        }
+
+    private:
+        GL::Mesh& _mesh;
+        Shaders::FlatGL2D& _shader;
+        Color4 _color;
+    };
+}
+
+#endif //MAGNUM_MOONLANDER_DRAWABLEMESH_H
